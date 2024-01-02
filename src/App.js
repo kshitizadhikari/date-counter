df2bdf1f22c397c93e470b77ccb5b9850f9083ae
seaweed78 <adhikarikshitiz12@gmail.com>
2024-01-02 16:16:04 +0545

    Count State Change

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [Count, changeCount] = useState(1);
+
+  function decreaseCount(count) {
+    if (Count > 1) changeCount((Count = Count - 1));
+  }
+  function increaseCount(count) {
+    if (Count < 100) changeCount((Count = Count + 1));
+  }
+
   return (
     <div className="container">
       <div className="mainBox">
-        <div className="stepBox">
+        <div className="subBox">
           <button className="decreaseBtn"> - </button>
           <p>Step : 2</p>
           <button className="increaseBtn"> + </button>
         </div>
-        <div className="countBox">
-          <button className="decreaseBtn"> - </button>
-          <p>Count : 1</p>
-          <button className="decreaseBtn"> + </button>
+        <div className="subBox">
+          <button className="decreaseBtn" onClick={decreaseCount}>
+            {" "}
+            -{" "}
+          </button>
+          <p>Count : {Count}</p>
+          <button className="increaseBtn" onClick={increaseCount}>
+            +
+          </button>
+        </div>
+        <div className="text">
+          <p>2 days from today is Thursday Jan 4 2023</p>
         </div>
       </div>
     </div>
